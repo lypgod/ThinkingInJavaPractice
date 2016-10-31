10686af2f350ddb5539367908b15c12cb5948f29
lypgod <lypgod@hotmail.com>
2016-10-31 16:56:51 +0800

    push from Inspiron15 on Oct.25

---
+++ src/main/java/com/lypgod/test/ThinkingInJava/Ch19_EnumeratedTypes/Practice11/Generator.java
@@ -0,0 +1,4 @@
+//: net/mindview/util/Generator.java
+// A generic interface.
+package com.lypgod.test.ThinkingInJava.Ch19_EnumeratedTypes.Practice11;
+public interface Generator<T> { T next(); } ///:~
